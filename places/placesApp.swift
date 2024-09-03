f63f4e21116757a5a1a3ff89c73a1bdea095ce4c
Vladyslav Deba <vdseam@gmail.com>
2024-09-03 21:07:38 +0200

    Initial Commit

---
+++ places/placesApp.swift
@@ -0,0 +1,17 @@
+//
+//  placesApp.swift
+//  places
+//
+//  Created by Vlad Deba on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct placesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
